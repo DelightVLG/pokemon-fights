bf36b48be8836b6e71a8c6da0e996ce8b29886a1
Sergei Kompanietc <delightvlg@gmail.com>
2021-04-20 10:52:05 +0300

    added log(console)

--- main.js
+++ main.js
@@ -46,6 +46,11 @@ function renderHp() {
 function changeHp(value) {
   this.damageHp -= value;
 
+  const log = this === enemy
+    ? generateLog(this.name, character.name, value, this.damageHp, this.defaultHp)
+    : generateLog(this.name, enemy.name, value, this.damageHp, this.defaultHp)
+  console.log(log);
+
   if (this.damageHp <= 0) {
     this.damageHp = 0;
     alert(`Покемон ${this.name} проиграл...`)
@@ -55,18 +60,34 @@ function changeHp(value) {
   this.renderHp();
 }
 
-function randomDmg(num) {
+function randomNum(num) {
   return Math.ceil(Math.random() * num);
 }
 
+function generateLog(firstPerson, secondPerson, damage, currentHp, maxHp) {
+  const logs = [
+    `${firstPerson} вспомнил что-то важное, но неожиданно ${secondPerson}, не помня себя от испуга, ударил в предплечье врага. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} поперхнулся, и за это ${secondPerson} с испугу приложил прямой удар коленом в лоб врага. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} забылся, но в это время наглый ${secondPerson}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} пришел в себя, но неожиданно ${secondPerson} случайно нанес мощнейший удар. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} поперхнулся, но в это время ${secondPerson} нехотя раздробил кулаком противника. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} удивился, а ${secondPerson} пошатнувшись влепил подлый удар. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} высморкался, но неожиданно ${secondPerson} провел дробящий удар. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} пошатнулся, и внезапно наглый ${secondPerson} беспричинно ударил в ногу противника -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} расстроился, как вдруг, неожиданно ${secondPerson} случайно влепил стопой в живот соперника. -${damage} [${currentHp}/${maxHp}]`,
+    `${firstPerson} пытался что-то сказать, но вдруг, неожиданно ${secondPerson} со скуки, разбил бровь сопернику -${damage} [${currentHp}/${maxHp}]`,
+  ];
+  return logs[randomNum(logs.length - 1)];
+}
+
 kickBtn.addEventListener('click', () => {
-  character.changeHp(randomDmg(20));
-  enemy.changeHp(randomDmg(20));
+  character.changeHp(randomNum(20));
+  enemy.changeHp(randomNum(20));
 });
 
 ultimateKickBtn.addEventListener('click', () => {
-  character.changeHp(randomDmg(70));
-  enemy.changeHp(randomDmg(70));
+  character.changeHp(randomNum(70));
+  enemy.changeHp(randomNum(70));
 })
 
 function init() {
